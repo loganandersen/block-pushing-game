371dc424d5db976356cc507723ec2fbb079fb46b
Aidan-Meshberg <ameshber@uccs.edu>
2024-12-03 00:13:37 -0700

    doing changes

--- level_grid.gd
+++ level_grid.gd
@@ -2,53 +2,25 @@ extends TileMapLayer
 
 # These are the atlas coordinates of each tile. The atlas coordinates are used
 # to refer to each tile in the tile map. 
-const PLAYER_TILE = Vector2i(1,0)
-const WALL_TILE = Vector2i(3,1)
-const BOX_TILE = Vector2i(0,1)
-const BUTTON = Vector2i(2,0)
-const GOAL = Vector2(3,0)
-const GOAL_CLOSED = Vector2(2,1)
+const LEVEL1 = Vector2i(0,0)
+const LEVEL2 = Vector2i(1,0)
+const LEVEL3 = Vector2i(2,0)
+const LEVEL4 = Vector2i(3,0)
+const LEVEL5 = Vector2i(0,1)
+const LEVEL6 = Vector2i(1,1)
+const LEVEL7 = Vector2i(2,1)
+const PLAYER_TILE = Vector2i(3,1)
 const EMPTY = Vector2i(-1,-1)
 
 #This is defined here because 
 # I am mainly worried about scope, when using the _ready function
 var playerLocation : Vector2i
-var buttonLocations : Array[Vector2i]
-var goalLocation : Vector2i 
 var backgroundGrid : TileMapLayer 
 
 # I will assume nothing in the background grid moves.
 func get_background_grid():
 	return get_node("../BackgroundGrid")
 
-func find_goal(): 
-	#Search for goals that are active
-	var goals = backgroundGrid.get_used_cells_by_id(0,GOAL)
-	# If there are no active goals search for an inactive one
-	if goals.is_empty():
-		goals = backgroundGrid.get_used_cells_by_id(0,GOAL_CLOSED)
-	# Get the goal, this will crash if the level has no goal.  
-	return goals[0]
-		
-
-#Check if there is a tile over every button
-func all_buttons_pushed() : 
-	# Check if any block above the button is empty
-	for position in buttonLocations :
-		print(position)
-		if get_cell_atlas_coords(position) == EMPTY:
-			
-			return false
-	return true
-		
-
-# Make the goal green if all buttons are pushed, make it black if no buttons
-# pushed.
-func update_goal() : 
-	if all_buttons_pushed() : 
-		backgroundGrid.set_cell(goalLocation, 0, GOAL)
-	else :
-		backgroundGrid.set_cell(goalLocation, 0, GOAL_CLOSED)
 
 func find_player_location() :
 	return get_used_cells_by_id(0,PLAYER_TILE)[0]
@@ -57,10 +29,7 @@ func find_player_location() :
 func _ready() -> void:
 	playerLocation = find_player_location()
 	backgroundGrid = get_background_grid()
-	# initialize goal
-	goalLocation = find_goal()
-	# Initialize buttons
-	buttonLocations = backgroundGrid.get_used_cells_by_id(0,BUTTON)
+
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -92,7 +61,6 @@ func _unhandled_input(event: InputEvent) -> void:
 				playerLocation = playerLocation + Vector2i(1,0)
 		
 		# Update tiles that have been affected by certain game conditions
-		update_goal()
 		
 
 #Attempt to move whatever is at from_coords to to_coords
@@ -100,14 +68,8 @@ func _unhandled_input(event: InputEvent) -> void:
 func attempt_move(from_coords, to_coords):
 	
 	#Stop on walls
-	if(get_cell_atlas_coords(to_coords) == WALL_TILE):
+	if(get_cell_atlas_coords(to_coords) != LEVEL1 || LEVEL2 || LEVEL3 || LEVEL4 || LEVEL5 || LEVEL6 || LEVEL7):
 		return false
-	
-	#Attempt to push boxes
-	if(get_cell_atlas_coords(to_coords) == BOX_TILE):
-		var was_pushed = attempt_move(to_coords, (2 * to_coords - from_coords))
-		if(!was_pushed): #Check if the box ran into something
-			return false
 	
 	#Actually move
 	# Set the tile we are supposed to move to to the tile from the previous 
