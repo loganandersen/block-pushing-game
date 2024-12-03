22b22d44cb25f333bbb76d95e4d701601446e720
Logan Andersen <landerse@uccs.edu>
2024-12-02 21:51:20 -0700

    added goal type stuff

--- game_grid.gd
+++ game_grid.gd
@@ -1,19 +1,44 @@
 extends TileMapLayer
 
+# These are the atlas coordinates of each tile. The atlas coordinates are used
+# to refer to each tile in the tile map. 
 const PLAYER_TILE = Vector2i(1,0)
 const WALL_TILE = Vector2i(3,1)
 const BOX_TILE = Vector2i(0,1)
+const BUTTON = Vector2i(2,0)
+const GOAL = Vector2(3,0)
+const GOAL_CLOSED = Vector2(2,1)
 
-var playerLocation = Vector2i(1,1)
+#This is defined here because 
+# I am mainly worried about scope, when using the _ready function
+var playerLocation : Vector2i
+var buttonLocations : Array[Vector2i]
+var goalLocation : Vector2i 
+var backgroundGrid : TileMapLayer 
+
+# I will assume nothing in the background grid moves.
+func get_background_grid():
+	return get_node("../BackgroundGrid")
+
+func find_goal(): 
+	#Search for goals that are active
+	var goals = backgroundGrid.get_used_cells_by_id(0,GOAL)
+	# If there are no active goals search for an inactive one
+	if goals.is_empty():
+		goals = backgroundGrid.get_used_cells_by_id(0,GOAL_CLOSED)
+	# Get the goal, this will crash if the level has no goal.  
+	return goals[0]
+		
 
 func find_player_location() :
 	return get_used_cells_by_id(0,PLAYER_TILE)[0]
-
 	
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	playerLocation = find_player_location()
-
+	backgroundGrid = get_background_grid()
+	goalLocation = find_goal()
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
